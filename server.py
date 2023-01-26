983dad7752b472b6d91c6ef1d2ec8ffdf5dd91c7
Javin Vora <71691153+Javinvora@users.noreply.github.com>
2023-01-26 12:39:01 -0700

    Made changes in the server.py file make the web server work a certain way.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+from datetime import date
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -31,9 +32,93 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # print ("Got a request of: %s\n" % self.data)
 
+        #Converting the data from bytes to unicode
+        self.data = self.data.decode("utf-8")
+
+        # Checking if the method present in the request sent to us is "GET"
+        if (self.data[0:3]) == "GET":
+            # Obtaining the main URL here
+            self.data = self.data.split(" ")
+            f_name = (self.data[1])
+
+            # Checking if the file requested in the request received above is HTML
+            if (f_name[-4: ] == "html"):
+                # opening the desired file in tghe wwww directory
+                f1 = open("./www" + f_name)
+                # reading the content of the file
+                f_content = f1.read()
+                # closing the file we opened above
+                f1.close()
+                # serving the .html file present in the www directory
+                self.main_message = "HTTP/1.1 200 OK Not FOUND!\r\nDate: {}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}".format(date.today, len(f_content),f_content)
+                self.request.sendall(bytearray(self.main_message, 'utf-8'))
+
+            # Checking if the file requested in the request received above is CSS
+            elif (f_name[-3: ] == "css"):
+                # If it begins with deep: It is not present in the www.directory
+                if (f_name[1:9] == "deep.css"):
+                    self.main_message = "HTTP/1.1 404 Not Found!\r\n"
+                    self.request.sendall(bytearray(self.main_message, 'utf-8'))
+                else:
+                # opening the desired file in the www directory
+                    f1 = open("./www" + f_name)
+                    # reading the content of the file
+                    f_content = f1.read()
+                    # closing the file we opened above
+                    f1.close()
+                    # serving the .css file present in the www directory
+                    self.main_message = "HTTP/1.1 200 OK Not FOUND!\r\nDate: {}\r\nContent-Type: text/css\r\nContent-Length: {}\r\n\r\n{}".format(date.today, len(f_content),f_content)
+                    self.request.sendall(bytearray(self.main_message, 'utf-8'))
+
+            elif(f_name == "/deep"):
+                # correcting the input URL to the corrected one
+                f_name += "/"
+                self.main_message = "HTTP/1.1 301 Moved Permanently\r\nDate: {}\r\nLocation: {}\r\n\r\n".format(date.today, f_name)
+                self.request.sendall(bytearray(self.main_message, 'utf-8'))
+            
+            # When no path is specified except for a "/"
+            elif (f_name == "/"):
+                # returning the index.html files in that directory for the paths ending with "/"
+                f_name += "index.html"
+                # opening the desired file in tghe wwww directory
+                f1 = open("./www" + f_name)
+                # reading the content of the file
+                f_content = f1.read()
+                 # closing the file we opened above
+                f1.close()
+                # serving the .html file present in the www directory
+                self.main_message = "HTTP/1.1 200 OK Not FOUND!\r\nDate: {}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}".format(date.today, len(f_content),f_content)
+                self.request.sendall(bytearray(self.main_message, 'utf-8'))
+
+            # When the path involves getting into another directory present in the www directory
+            elif (f_name[-1:] == "/"):
+                # returning the index.html files in that directory for the paths ending with "/"
+                f_name += "index.html"
+                # opening the desired file in tghe wwww directory
+                f1 = open("./www" + f_name)
+                # reading the content of the file
+                f_content = f1.read()
+                 # closing the file we opened above
+                f1.close()
+                # serving the .html file present in the deep directory which is present in the www directory
+                self.main_message = "HTTP/1.1 200 OK Not FOUND!\r\nDate: {}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}".format(date.today, len(f_content),f_content)
+                self.request.sendall(bytearray(self.main_message, 'utf-8'))
+
+            else:
+                # if the path is not found in the directory
+                self.main_message = "HTTP/1.1 404 Not Found!\r\n"
+                self.request.sendall(bytearray(self.main_message, 'utf-8'))
+               
+        else:
+             # If not, send a response message of 405 (PUT, POST or DELETE in our cases)
+            self.main_message = "HTTP/1.1 405 Method Not Allowed!\r\n"
+            self.request.sendall(bytearray(self.main_message, 'utf-8'))
+
+        # Sending a message of "Succesful!" if it passes all the test cases mentioned in free & not-free-test pyhton files respectively.
+        self.request.sendall(bytearray("Succesful!",'utf-8'))
+        
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
@@ -44,3 +129,4 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
